75215810820bacd78bf371668fcf7a43938b2e71
katy@karstaway.com <katy@karstaway.com>
2018-09-04 20:03:40 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,7 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year
   FROM books
-  INNER   JOIN series
+  INNER JOIN series
+  ON books.id = series.book
   "
 "SELECT users.name, SUM(amount)
 FROM pledges
