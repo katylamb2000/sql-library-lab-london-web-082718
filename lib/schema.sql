7e17c7cc437720872d4051951ddfc3ea22d7651b
katy@karstaway.com <katy@karstaway.com>
2018-09-04 19:58:27 +0000

    automatically backed up by learn

---
+++ lib/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE series (id PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
+CREATE TABLE subgenres (id PRIMARY KEY, name TEXT);
+CREATE TABLE authors (id PRIMARY KEY, name TEXT);
+CREATE TABLE books (id PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
+CREATE TABLE characters (id PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);
+CREATE TABLE character_books (id PRIMARY KEY, book_id INTEGER, character_id INTEGER);
